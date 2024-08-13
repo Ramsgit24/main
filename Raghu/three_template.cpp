20b6340d0830be2eaf71f66c02dd15899f629608
Raghu <ramsgithub24@gmail.com>
2024-08-13 18:47:26 +0530

    Raghu:template_function

---
+++ Raghu/three_template.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+template<typename Q,typename U>
+U sum(Q , U);
+template<typename P,typename R>
+P add(P a,R b)
+{
+	P c;
+	c=a+b;
+	return c;
+
+}
+template<typename T>
+T add(T a,T b)
+{
+	T c;
+	c=a+b;
+	return c;
+}
+int main()
+{
+	int a=add<int>(10,20);
+	float b=add<float>(10.20f,20.20f);
+	char c=add<char>('a','b');
+	int d=add<int,float>(10,20.3f);
+	float e=add<float,int>(20.3f,10);
+	float f=sum<int,int>(10,30);
+	cout<<"addition value are"<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e<<" "<<f<<endl;
+}
+template<typename Q,typename U>
+U sum(Q a,U b)
+{
+	U c;
+	c=a+b;
+	return c;
+}
