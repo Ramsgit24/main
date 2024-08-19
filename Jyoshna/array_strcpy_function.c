dddc601ba01f1dccf5b0eebd523d71f6bcb58423
L-ikitha <bhaskarlikitha705@gmail.com>
2024-08-19 18:48:57 +0530

    Jyoshna: new string function

---
+++ Jyoshna/array_strcpy_function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void ur_strcpy(char[],char[]);
+void main()
+{
+	char s[]="hello";
+	char d[7];
+	ur_strcpy(d,s);
+}
+void ur_strcpy(char d[],char s[])
+{
+	int i=0;
+	while(s[i]!='\0')
+	{
+		d[i]=s[i];
+		i++;
+	}
+	d[i]='\0';
+	printf("%s\n",d);
+}
