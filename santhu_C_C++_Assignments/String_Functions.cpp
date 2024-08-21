99fea2bb46a29aba9ff16904eab66955d4bc09fc
santhoshn-vrize <Santhosh@vrize.com>
2024-08-21 08:23:39 +0530

    santhu: optimization string functions and logic changed

--- santhu_C_C++_Assignments/String_Functions.cpp
+++ santhu_C_C++_Assignments/String_Functions.cpp
@@ -1,88 +1,109 @@
-#include <stdio.h>
-#include <string.h>
-using namespace std;
-int ur_strlen(char a[]){
-	int len = 0;
-	while(a[len] != '\0'){
-		len++;
-	}
-	return len; 
+#include<stdio.h>
+int ur_strlen(char []);
+void ur_strcpy(char [],char []);
+void ur_strcat(char [],char []);
+int ur_strcmp(char [],char []);
+void ur_strrev(char []);
+int main()
+{
+	char a[]="santhu";
+	char b[20]="";
+	char c[10]="santhu";
+	char d[20]="santhu";
+	char e[10]=" embedded";
+	char f[10]="bharath";
+	char g[10]="bharath";
+	char h[7]="uhtnas";
+	int length;
+	int stringcmp;
+	length = ur_strlen(a);
+	printf("Length Of The String %d\n",length);
+	ur_strcpy(c,b);
+	printf("String Copy %s\n",b);
+	ur_strcat(d,e);
+	printf("String Copy %s\n",d);
+	stringcmp = ur_strcmp(f,g);
+	printf("String Comparision %d\n",stringcmp);
+	ur_strrev(h);
+	printf("String Reverse %s\n",h);
+	return 0;
 }
-char* ur_strcpy(char d[],char s[]){
-	int len = 0;
-	int i = 0,j=0;
-	while(d[i] != '\0'){
-		s[j] = d[i];
+int ur_strlen(char a[])
+{
+	int i;
+	i=0;
+	while(a[i] != '\0'){
 		i++;
-		j++;
-
 	}
-	s[j] = '\0';
-	return s; 
+	return i;
 }
-char* ur_strcat(char s[],char d[]){
-
-	int i=0,len=0;
-	while(s[len] != '\0'){
-		len++;
+void ur_strcpy(char d[],char s[])
+{
+	int len;
+	int i;	
+	i=0;
+	while(d[i]!=0)
+	{
+		s[i] = d[i];
+		i++;
+		
 	}
-
-	while(d[i] != '\0'){
-		s[len] = d[i];
-		len++,i++;
-	}
-	return s;
 }
-int ur_strcmp(char a[],char b[]){
-	int i=0,len=0;
-	while(a[len] != '\0'){
+void ur_strcat(char d[],char s[])
+{
+	//d=santhu
+	//s=embedded
+	int len;
+	int i;	
+	i=0;
+	len = ur_strlen(d);
+	while(s[i] != '\0')
+	{
+		d[len] = s[i];
 		len++;
+		i++;
 	}
-	while(i<len){
-		if(a[i] != b[i]){
-			return i;
+}
+int ur_strcmp(char s[],char d[])
+{
+	//santhu
+	//santku
+	int len;
+	int i;
+	int result;
+	result = 0;
+	i=0;
+	
+	while(s[i] != '\0' || d[i] != '\0')
+	{
+		if(s[i] != d[i])
+		{
+			result = 1;
+			break;
 		}
-		else{
+		else
+		{
 			i++;
 		}
 	}
-	return 0; 
+	
+	return result;
 }
-char* ur_strrev(char a[]){
-	int i=0,len=0,j;
-
-	while(a[len] != '\0'){
-		len++;
-	}
-
-	j=len-1;
-	while(i<len/2){
-		char temp = a[i];
-		a[i] = a[j];
-		a[j] = temp;
+void ur_strrev(char s[])
+{
+	int len;
+	int i;
+	int j;
+	char temp;
+	len = ur_strlen(s);
+	i=0;
+	j=len - 1;
+	while(s[i] != '\0' && i<j)
+	{		
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
 		i++;
 		j--;
 	}
-	return a; 
-}
-int main()
-{
-	char a[] = "RamCharan";
-	int aLength = ur_strlen(a);
-	printf("String Length -> %d\n",aLength);
-	char s[] = "";
-	char d[] = "Pawanism";
-	ur_strcpy(d,s);
-	printf("Copy String Destination to Source -> %s\n",s);
-	char e[20] = "Pawanism";
-	char f[] = " OG";
-	ur_strcat(e,f);
-	printf("String ConCatination -> %s\n",e);
-	char g[] = "naylaknawap";
-	ur_strrev(g);
-	printf("Reverse String -> %s\n",g);
-	char h[] = "pawan";
-	char i[] = "pawan";
-	printf("Reverse String -> %d\n",ur_strcmp(h,i));
-	return 0;
 }
