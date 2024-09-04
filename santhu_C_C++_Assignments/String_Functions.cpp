c6f40bf77d3e364aa95c608759b1ec7d7f560c9b
santhoshn-vrize <Santhosh@vrize.com>
2024-09-04 20:09:43 +0530

    santhu: updated pointers

--- santhu_C_C++_Assignments/String_Functions.cpp
+++ santhu_C_C++_Assignments/String_Functions.cpp
@@ -20,7 +20,7 @@ int main()
 	printf("Length Of The String %d\n",length);
 	ur_strcpy(c,b);
 	printf("String Copy %s\n",b);
-	ur_strcat(d,e);
+	ur_strcat(d,e);//d=1000 e=2000//d address we are passing//e address is passing
 	printf("String Copy %s\n",d);
 	stringcmp = ur_strcmp(f,g);
 	printf("String Comparision %d\n",stringcmp);
@@ -46,27 +46,31 @@ void ur_strcpy(char* d,char* s)
 	while(*d != '\0')
 	{
 		*s = *d;
-        d++;
-        s++;
-		
+		d++;
+		s++;
+
 	}
 }
-void ur_strcat(char* d,char* s)
+void ur_strcat(char* a,char* b)
 {
 	//d=santhu
 	//s=embedded
 	int len;
 	int i;	
 	i=0;
-	len = ur_strlen(d);
-	d+=len;
-	while(*s != '\0')
+	//len = ur_strlen(d);
+	//d+=len;
+	while(*a!='\0')
 	{
-		*d = *s;
-		d++;
-		s++;
+		a++;//increasing address //char is 1 byte a++//int is 4 byte a++//float is 4 byte by a++
 	}
-	*d='\0';
+	while(*b != '\0')
+	{
+		*a = *b;
+		a++;//increasing address //char is 1 byte a++//int is 4 byte a++//float is 4 byte by a++
+		b++;//increasing address //char is 1 byte b++//int is 4 byte b++//float is 4 byte by b++
+	}
+	*b='\0';
 }
 int ur_strcmp(char* s,char* d)
 {
@@ -76,7 +80,7 @@ int ur_strcmp(char* s,char* d)
 	int result;
 	result = 0;
 	i=0;
-	
+
 	while(*s != '\0' || *d != '\0')
 	{
 		if(*s != *d)
@@ -86,22 +90,24 @@ int ur_strcmp(char* s,char* d)
 		}
 		else
 		{
-		d++;
-		s++;
+			d++;
+			s++;
 		}
 	}
-	
+
 	return result;
 }
 void ur_strrev(char* s)
 {
-	int len;
-	char* i;
-	char* j;
+
+	char* i=s;
+	char* j=s;
 	char temp;
-	len = ur_strlen(s);
-	i=s;
-	j=s + len - 1;
+	while(*j!='\0')
+	{
+		j++;
+	}
+	j--;
 	while(i<j)
 	{		
 		temp = *i;
