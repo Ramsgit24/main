13be903a67811280bd2f55649a88daa713b238f4
Pavani Karamala <ramsgithub24@gmail.com>
2024-09-09 18:22:19 +0530

    Ganesh:displaying array elements

---
+++ Ganesh/array/array/array.c
@@ -0,0 +1,67 @@
+////#include<stdio.h>
+////void enterarray();
+////void displayarray();
+////int a[5],i;
+////int main()
+////{
+////	enterarray();
+////	displayarray();
+////
+////}
+////void enterarray()
+////{
+////	printf("enter array elements :\n ");
+////	for(i=0;i<5;i++)
+////	{
+////		printf("Enter the value for arr[%d]= ",i);
+////		scanf("%d",&a[i]);
+////	}
+////}
+////
+////void displayarray()
+////{
+////	printf("\nDisplaying array elements are :\n ");
+////	for(i=0;i<5;i++)
+////	{
+////		printf("%d\t",a[i]);
+////	}
+////	printf("\n");
+////}
+//
+//#include<stdio.h>
+//void enterarray();
+//void displayarray();
+//int a[5],i;
+//int main()
+//{
+//	enterarray();
+//	displayarray();
+//}
+//void enterarray()
+//{
+//printf("enter array element :\n");
+//for(i=0;i<5;i++)
+//{
+//	printf("enter the values for array[%d]=",i);
+//		scanf("%d,&a[i]");
+//}
+//}
+//void displayarray()
+//{
+//	printf("displaying of array elements are :");
+//	for(i=0;i<5;i++)
+//	{
+//		printf("%d\t",a[i]);
+//	}
+//	printf("\n")
+//}
+//
+
+#include<stdio.h>
+int main()
+{
+	int i,a[5]={10,20,30,40,50};
+	for(i=1;i<5;i++)
+	{
+		printf()
+
