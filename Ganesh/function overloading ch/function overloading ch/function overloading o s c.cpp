f159df8a46f7cf903b4d9e214ad8d69ba7f7f52d
Pavani Karamala <ramsgithub24@gmail.com>
2024-09-12 11:50:53 +0530

    Ganesh:function overload with class and without class

--- Ganesh/function overloading ch/function overloading ch/function overloading o s c.cpp
+++ Ganesh/function overloading ch/function overloading ch/function overloading o s c.cpp
@@ -1,36 +1,82 @@
+//#include<iostream>
+//using namespace std;
+//void add(int ,int);
+//void sub(int);
+//void letter(char,char);
+//void mark(float,float);
+//int main()
+//{
+//	add(30,40);
+//	sub(20);
+//	letter('G','A');
+//	mark(1.8f,2.8f);
+//}
+//void add(int a,int b)
+//{
+//	cout<<"c="<<a+b<<endl;
+//}
+//void sub(int c)
+//{
+//	int a=20;
+//	int d;
+//	d=a-c;
+//	cout<<"d="<<d<<endl;
+//}
+//void letter(char h,char y)
+//{
+//	cout<<"a="<<h<<" "<<y<<endl;
+//	
+//}
+//void mark(float a,float b)
+//{
+//	float f=a/b;
+//	cout<<"f="<<f<<endl;
+//}
+//
+//
+
+
 #include<iostream>
 using namespace std;
-void add(int ,int);
-void sub(int);
-void letter(char,char);
-void mark(float,float);
+class A
+{
+public:
+	void len(char*);
+	void copy(char*,char*);
+
+};
+void A::copy(char *a,char *b)
+{
+	
+	while(*b!='\0')
+	{
+		*a=*b;
+		b++;
+		a++;
+	}
+	*a='\0';
+	//cout<<a<<endl;
+}
+
+
+
+void A::len(char *a)
+{
+	int i=0;
+	while(a[i]!='\0')
+	{
+		i++;
+	}
+	cout<<i<<endl;
+
+
+}
 int main()
 {
-	add(30,40);
-	sub(20);
-	letter('G','A');
-	mark(1.8f,2.8f);
-}
-void add(int a,int b)
-{
-	cout<<"c="<<a+b<<endl;
-}
-void sub(int c)
-{
-	int a=20;
-	int d;
-	d=a-c;
-	cout<<"d="<<d<<endl;
-}
-void letter(char h,char y)
-{
-	cout<<"a="<<h<<" "<<y<<endl;
-	
-}
-void mark(float a,float b)
-{
-	float f=a/b;
-	cout<<"f="<<f<<endl;
-}
+	A obj1;
+	obj1.len("Ganesh");
+	char c[10];
+	obj1.copy(c,"chandu ");
+	cout<<c<<endl;
 
-
+}
