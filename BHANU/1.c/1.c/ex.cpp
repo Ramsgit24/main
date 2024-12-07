1515958fa61b61eaa99eca04eacd16f0d66796f6
Bhanu <bhanuprakashkota0.com>
2024-12-07 09:02:44 +0530

    adding all files

--- BHANU/1.c/1.c/ex.cpp
+++ BHANU/1.c/1.c/ex.cpp
@@ -1,33 +1,71 @@
+//#include<stdio.h>
+//int factorial(int);
+//int main(){
+//	int x,y;
+//	scanf("%d",&x);
+//	y = factorial(x);
+//	printf("%d",y);
+//}
+//int factorial(int fact){
+//	if(fact==0 || fact ==1){
+//		return 1;
+//	}
+//	else{
+//		fact * factorial(fact - 1);
+//	}
+//}
+
+
+
+
+
 #include <iostream>
+
+#include <iomanip>
 using namespace std;
-template<typename t>
-class A{
-    t x;
-    t *ptr;
-    t result;
+class Calendar {
+private:
+    int month, year;
+    string months[12] = {"January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"};
+    int days[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 public:
-    A(t a,t b){
-        x = a;
-        ptr = new t(b);
-        result = add(x,*ptr);
+    // Constructor
+    Calendar(int m, int y) {
+        month = m;
+        year = y;
+        if (isLeapYear()) days[1] = 29;
     }
-   t add(t a,t b){
-        return a + b;
-   }
-    ~A(){
-        delete ptr;
+
+    // Check leap year
+    bool isLeapYear() {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
     }
-    void display(){
-    cout<<result<<endl;
+
+    // Print calendar
+    void printCalendar() {
+        cout << "\t" << months[month - 1] << " " << year << endl;
+        cout << "Su Mo Tu We Th Fr Sa" << endl;
+
+        int day = 1;
+        for (int i = 0; i < days[month - 1]; i++) {
+            cout << setw(3) << day;
+            if ((i + 1) % 7 == 0) cout << endl;
+            day++;
+        }
     }
 };
+
 int main() {
-    A<int>obj1(10,20);
-	A<float> obj2(22.33,44.55);
-	A<double>obj3(11.22,22);
-	A<char>obj4('a','b');
-    obj1.display();
-	obj2.display();
-	obj3.display();
-	obj4.display();
+    int month, year;
+    cout << "Enter month (1-12): ";
+    cin >> month;
+    cout << "Enter year: ";
+    cin >> year;
+
+    Calendar cal(month, year);
+    cal.printCalendar();
+
+    return 0;
 }
+
